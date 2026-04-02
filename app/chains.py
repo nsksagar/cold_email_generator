e5bc6c3ccbe682203d950bda8acf3961cba52711
Sagar Nookarapu <193294266+nsksagar@users.noreply.github.com>
2026-04-02 19:55:32 -0400

    final application

--- app/chains.py
+++ app/chains.py
@@ -8,13 +8,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.getenv("api_key")
+my_api_key = os.getenv("my_api_key")
 
 class Chain:
 
     def __init__(self):
-        self.llm =  ChatGroq(model= "llama-3.3-70b-versatile",api_key = my_api_key,temperature=0,
-)
+        self.llm =  ChatGroq(model= "llama-3.3-70b-versatile",api_key = my_api_key,temperature=0)
         
     def extract_jobs(self, cleaned_text):
 
@@ -31,3 +30,46 @@ class Chain:
 
                             """
                             )
+        
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input = {'page_data' : cleaned_text})
+
+        try:
+            json_parser = JsonOutputParser()
+
+            res = json_parser.parse(res.content)
+
+        except OutputParserException:
+            raise OutputParserException('Context too big. Unable to parse jobs')
+        
+
+        return res if isinstance(res, list) else [res]
+    
+    def write_mail(self, job, links):
+
+        prompt_email = PromptTemplate.from_template(
+                    """
+                    ### JOB DESCRIPTION 
+                    {job_description}
+
+                    ### INSTRUCTION: 
+                    You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software consulting company dedicates 
+                    the seamless integration of business processes through automated tools.
+                    Over our experience, we have empowered numerous enterprices with talented solutions, fostering scalability,
+                    process optimization, cost reduction, and heightened overall efficiency. 
+                    Your job is to write a cold email to the client regarding the job mentioned above describing the capability of Atliq
+                    in fulfilling their needs. 
+
+                    Also add the most relevant ones from the following the links to showcase Atliq's portfolio : {link_list}
+                    Remember you are Mohan, BDE at AtliQ.
+                    Do not provide a preamble. 
+                    #### EMAIL (NO PREAMBLE) : 
+                    """
+                )
+        
+        chain_email = prompt_email | self.llm 
+        res = chain_email.invoke({'job_description' : str(job), 'link_list': links})
+        
+    
+        
+
--- app/main.py
+++ app/main.py
@@ -1,11 +1,37 @@
 import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
 
-st.title('Cold Email generator')
-url_input = st.text_input('Enter a URL :', value = "https://jobs.boehringer-ingelheim.com/job/Ridgefield%2C-CT-CMC-Regulatory-Data-Science-Co-Op-%28Hybrid%29-Unit/1368819133/")
-submit_button = st.button("Submit")
+from chains import Chain 
+from portfolio import portfolio
+from utils import clean_text
 
-if submit_button:
-    st.code('Hello Hiring Manager, I am from AtliQ', language = 'markdown')
+def create_streamlit_app(llm, portfolio, clean_text):
+
+    st.title('Cold email Generator')
+
+    url_input = st.text_input('Enter a URL:', value = "https://jobs.boehringer-ingelheim.com/job/Ridgefield%2C-CT-CMC-Regulatory-Data-Science-Co-Op-%28Hybrid%29-Unit/1368819133/")
+    submit_button = st.button('Submit')
+
+    if submit_button:
+        try: 
+            loader = WebBaseLoader([url_input])
+            data = clean_text(loader.load().pop().page_content)
+            portfolio.load_portfolio()
+            jobs = llm.extract_jobs(data)
+            for job in jobs: 
+                skills = job.get('skills', [])
+                links = portfolio.query_links(skills)
+                email = llm.write_mail(jobs, links)
+                st.code(email, language = 'markdown')
+        except Exception as e:
+            st.error(f'An Error Occured: {e}')
+
+if __name__ == '__main__':
+
+    chain = Chain()
+    portfolio = portfolio()
+    st.set_page_config(layout = 'wide', page_title='Cold Email Generator', page_icon=  "✉️")
+    create_streamlit_app(chain, portfolio, clean_text)
 
 
 
